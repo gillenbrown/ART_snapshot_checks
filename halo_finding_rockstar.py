fcb104d8a3db39955611c25137f64c0a9f6ac5c5
Gillen Brown <gillenbrown@gmail.com>
2019-05-21 09:17:37 -0400

    have rockstar restart, rather than rebuilding catalogs all the time

--- halo_finding_rockstar.py
+++ halo_finding_rockstar.py
@@ -12,23 +12,18 @@ yt.enable_parallelism()
 # idx 0: name of script
 # ids 1: directory of simulation outputs to run halos on
 # idx 2: directory to store halo finding outputs in
-# idx 3: must be `remove`, `silent` or not present
+# idx 3: must be `silent` or not present
 
 # do error checking on root only.
 if yt.is_root():
     if len(sys.argv) < 3:
         raise ValueError("Please provide the proper command line argument.")
-    elif len(sys.argv) == 4:
-        if sys.argv[3] != "remove":
+    elif len(sys.argv) == 3:
+        if sys.argv[3] != "silent":
             raise ValueError("If present, the third command line "
-                             "argument can only be `remove`")
-    elif len(sys.argv) > 4:
+                             "argument can only be `silent`")
+    elif len(sys.argv) > 3:
         raise ValueError("Extra command line arguments not recognized.")
-    # check the order. The input will be checked elsewhere, so we only have to 
-    # check remove
-    if sys.argv[2] == "remove":
-        raise ValueError("The third command line argument must be `remove`, "
-                         "not the second.")
 
 # turn the directories the user passes into the absolute path
 sim_dir = os.path.abspath(sys.argv[1])
@@ -39,16 +34,13 @@ if not out_dir.endswith(os.sep):
     out_dir += os.sep
 
 # check to see if there is a currently existing halo catalog already here
-# again, do this only on the root.
+# to restart from. Again, do this only on the root.
 if yt.is_root():
     if os.path.exists(out_dir):
-        if len(sys.argv) == 3:  # remove not present
-            raise ValueError("Existing halo catalogs already present.\n"
-                             "            Use keyword `remove` to overwrite.")
-        else:  # user does want to remove
-            shutil.rmtree(out_dir)
+        restart = True
     else:
         os.mkdir(out_dir)
+        restart = False
 
 if yt.is_root():
     print("Reading simulations from: {}".format(sim_dir))
@@ -66,7 +58,7 @@ else:
 # process too.
 rh = RockstarHaloFinder(ts, num_readers=2, num_writers=17, outbase=out_dir,
                         particle_type=particle_type)
-rh.run()
+rh.run(restart=restart)
 
 # update the sentinel file
 pathlib.Path(out_dir + "sentinel.txt").touch()
