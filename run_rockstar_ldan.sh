f62204b1d3c5e6eec88128ed6af0a5e2ee2aacd2
Gillen Brown <gillenbrown@gmail.com>
2019-11-01 14:31:10 -0400

    changed number of cores requested per rockstar instance by parent script

--- run_rockstar_ldan.sh
+++ run_rockstar_ldan.sh
@@ -2,5 +2,6 @@
 # Set some environment variables so MPI is sure to be launched properly
 export SEVERAL_TRIES_NTRIES=10
 export MPI_LAUNCH_TIMEOUT=300 
-# Lou analysis machines have Skylake nodes with 20 cores
-/pleiades/u/scicon/tools/bin/several_tries mpiexec -np 20 python ./halo_finding_rockstar.py $1 $2 $3
+# Lou analysis machines have Skylake nodes with 20 cores, where we have 
+# 4 processes using 5 cores
+/pleiades/u/scicon/tools/bin/several_tries mpiexec -np 5 python ./halo_finding_rockstar.py $1 $2 $3
