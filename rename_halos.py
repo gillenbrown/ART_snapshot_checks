436f9ff6895d270b1c80b652d24af2743e201c19
Gillen Brown <gillenbrown@gmail.com>
2019-12-14 14:06:55 -0500

    fixes to rename halos

--- rename_halos.py
+++ rename_halos.py
@@ -26,6 +26,20 @@ if not halo_output_dir.endswith(os.sep):
 # get the scale factor of the desired halo catalog
 target_a_str = halo_output_file[-12:-6]
 
+# create function to check a given halo catalog against the expected value
+def file_matches_scale_factor(input_file, target_a_str):
+    with open(rockstar_dir + file, "r") as halo:
+        # first line is not needed
+        halo.readline()
+        # second line has scale factor
+        this_a_str = halo.readline().split()[-1]
+
+        # python format automatically rounds so we can compare it to the
+        # scale factor in the target str
+        this_a_str = "{:.4f}".format(float(this_a_str))
+
+        return this_a_str == target_a_str 
+
 # go through all the rockstar outputs to find the desired one. In the end we'll 
 # store the prefix for those files, so for now we'll set a sentinel value 
 # telling us we didn't find it.
@@ -34,34 +48,42 @@ for file in os.listdir(rockstar_dir):
     # Each output has multiple halo files. Get only one of them to read in
     # and test. The ASCII files have the scale factor right in them
     if "halos_" in file and ".0.ascii" in file:
-        with open(rockstar_dir + file, "r") as halo:
-            # first line is not needed
-            halo.readline()
-            # second line has scale factor
-            this_a_str = halo.readline().split()[-1]
+        if file_matches_scale_factor(file, target_a_str):
+            # get everything up to the period. This will contain the file
+            # index that matches what we want. We do want the period, though, so
+            # that we don't include halos_80.0.ascii when we found 
+            # halos_8.0.ascii as the match
+            old_prefix = file.split(".")[0] + "."
+            break
 
-            # python format automatically rounds so we can compare it to the
-            # scale factor in the target str
-            # there is one pathological case where the rounding breaks 
-            # because of floating point inaccuracies
-            if this_a_str == "0.068650":
-                this_a_str = "0.0686"
-            else:
-                this_a_str = "{:.4f}".format(float(this_a_str))
+# there is one pathological case where the rounding breaks.
+# I think this is because the halo output file already reports a 
+# rounded value, and rounding that can case inaccuracies
+# i.e. a=0.20004999 should be rounded to a=0.2000, but the halo file
+# rounds to more digits, reports a=0.200050, which then gets rounded
+# to a=0.2001. There's also the 0.5 rounding to even thing that 
+# I'm not sure if that happens in ART, so I'll check one digit on 
+# either side to see if those match too
+if old_prefix == "not found":
+    print(" - {} is a weird case".format(halo_output_file))
+    temp_target_str_up =   "{:.4f}".format(float(target_a_str) + 0.0001)
+    temp_target_str_down = "{:.4f}".format(float(target_a_str) - 0.0001)
+    # then do the same looking we did before
+    for file in os.listdir(rockstar_dir):
+        if "halos_" in file and ".0.ascii" in file:
+            if file_matches_scale_factor(file, temp_target_str_up) or \
+               file_matches_scale_factor(file, temp_target_str_down):
 
-            if this_a_str == target_a_str:
-                # get everything up to the period. This will contain the file
-                # index that matches what we want
-                old_prefix = file.split(".")[0]
+                old_prefix = file.split(".")[0] + "."
                 break
 
-# if we didn't find it, exit
-if old_prefix == "not found":
+# if we still can't find it, exit
+if old_prefix == "not found": 
     raise ValueError("Rockstar halo file for\n{}\nwas not found in "
                      "directory\n{}".format(halo_output_file, rockstar_dir))
 
 # now we have the right index and can copy all the files we need
-new_prefix = "halos_a{}".format(target_a_str)
+new_prefix = "halos_a{}.".format(target_a_str)
 for file in os.listdir(rockstar_dir):
     if file.startswith(old_prefix):
         old_path = rockstar_dir + os.sep + file
