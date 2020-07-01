72a2ff13ecd09138d262c30808ac9c328fc274c3
Gillen Brown <gillenb@umich.edu>
2020-07-01 15:38:49 -0400

    finalized submit script

--- submit.sh
+++ submit.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-user=gillenb@umich.edu
 #SBATCH --mail-type=ALL
 #SBATCH --no-requeue
-#SBATCH --partition=development
-#SBATCH --time=00:00:01
+#SBATCH --partition=normal
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=68
@@ -18,11 +18,6 @@ module load remora python3/3.7.0
 
 # go to the directory I submitted the job from
 cd $SLURM_SUBMIT_DIR
-
-# need to disable pinning to avoid having multiple processes running on same
-# set of CPUs
-#export MPI_DSM_VERBOSE=1
-#export MPI_DSM_DISTRIBUTE=0
 
 # so far I have one production run. I'll need 5 cores for that halo finding.
 # that leaves the rest for other things, so I can use 64 parallel ranks
