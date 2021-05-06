83a5bb5f62b51df42db8560b3d5621264daa7148
Gillen Brown <gillenbrown@gmail.com>
2021-05-06 14:10:38 -0400

    proper names for Thelma and Louise runs on plots

--- plot_utils.py
+++ plot_utils.py
@@ -19,11 +19,13 @@ names = {
     full_dir("stampede2/old_ic_comparison/cap5000kms_hn20/run"): "Old IC SFE100 20% HN",
     full_dir("stampede2/old_ic_comparison/cap5000kms_hn50_v1/run"): "Old IC SFE100 50% HN",
     full_dir("stampede2/old_ic_comparison/cap5000kms_hn50/run"): "Old IC SFE100 50% HN new",
-    full_dir("stampede2/production/tl_sfe001_hn20/run"): "LG SFE1 20% HN",
-    full_dir("stampede2/production/tl_sfe010_hn20/run"): "LG SFE10 20% HN",
-    full_dir("stampede2/production/tl_sfe100_hn20/run"): "LG SFE100 20% HN",
-    full_dir("stampede2/production/tl_sfe100_hn05/run"): "LG SFE100 5% HN",
-    full_dir("stampede2/production/tl_sfe100_hn00/run"): "LG SFE100 0% HN",
+    full_dir("stampede2/production/tl_sfe001_hn20/run"): "T&L SFE1 20% HN",
+    full_dir("stampede2/production/tl_sfe010_hn20/run"): "T&L SFE10 20% HN",
+    full_dir("stampede2/production/tl_sfe100_hn20/run"): "T&L SFE100 20% HN",
+    full_dir("stampede2/production/tl_sfe100_hn05/run"): "T&L SFE100 5% HN",
+    full_dir("stampede2/production/tl_sfe100_hn00/run"): "T&L SFE100 0% HN",
+    full_dir("stampede2/production/rj_sfe010_hn20/run"): "R&J SFE10 20% HN",
+    full_dir("stampede2/production/rj_sfe100_hn20/run"): "R&J SFE100 20% HN",
 }
 
 cmap_tl_sfe = cm.Blues
