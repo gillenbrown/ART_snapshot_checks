a373d7b3bdb70b3ae7f1f42de41d92f5275f3f05
Gillen Brown <gillenb@umich.edu>
2021-06-23 07:52:40 -0400

    stampede2 - removed moving to scratch
    That didn't really do anything meaningful to reduce IO, and was a hassle

--- submit.sh
+++ submit.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --no-requeue
 #SBATCH --partition=skx-normal
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
 #SBATCH --cpus-per-task=1
@@ -24,14 +24,6 @@ unset PYTHONPATH
 # echo $(which python)
 # echo $PYTHONPATH
 
-# I then need to copy the whole setup to scratch so I can run the job from
-# there. This is required by the Stampede2 folks to reduce stress on the
-# $WORK filesystem
-work_dir=$WORK/ART_snapshot_checks/
-new_dir=$SCRATCH/snapshot_checks_${SLURM_JOB_ID}
-cp -r $work_dir $new_dir
-cd $new_dir
-
 # halos uses all the cores, so it needs to be done one at a time. The number
 # used thereafter depends on the memory, it's just empirical. For halos we have
 # one master process for make. The halo finding script will use ibrun to spawn
@@ -41,8 +33,3 @@ make dirs
 remora ibrun -n 1 -o 0 make halos
 remora make -j6
 # &>> $SLURM_JOB_NAME.stdout.$SLURM_JOB_ID
-
-# Then I can remove our temporary directory. No plots should be written here,
-# as everything is in the simulation directories, and the stdout file will be
-# in the submission directory (i.e. $WORK)
-rm -rf $new_dir
