8b1ec7fcdf071b238a3b0074b46d648013b92967
Gillen Brown <gillenb@umich.edu>
2021-09-24 16:04:13 -0400

    simplified analysis_all

--- analysis_all.py
+++ analysis_all.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import gc
 import numpy as np
 import yt
 
@@ -22,8 +23,8 @@ directories = [
 ]
 
 
-def write(run_name, redshift, value1, value2):
-    this_str = f"{run_name} {redshift:.4f} {value1:.3e} {value2:.3e}\n"
+def write(run_name, redshift, value1):
+    this_str = f"{run_name} {redshift:.4f} {value1:.3e}\n"
     out_file.write(this_str)
     out_file.flush()  # this and next line ensure writing happens now
     os.fsync(out_file.fileno())
@@ -42,10 +43,6 @@ for d in directories:
 
     # then parse this to get the halo files
     for output in art_files:
-        print("", flush=True)
-        print(output, flush=True)
-        print("", flush=True)
-
         # try:
         sim = load_galaxies.Simulation(output, sphere_radius_kpc=30, n_galaxies=1)
         # except:
@@ -57,35 +54,20 @@ for d in directories:
 
         ad = sim.ds.all_data()
 
-        # Then do analysis. This is copied from another analysis script
-        # check that we have any halos at all. If not, we can exit. This can happen
-        # for early outputs where nothing has collapsed yet.
-        if len(sim.galaxies) == 0:
-            write(run_name, sim.z, np.nan, np.nan)
-            continue
-
-        # only work on the biggest halo
-        for gal in sim.galaxies:
-            if gal.rank == 1:
-                break
-
-        assert gal.rank == 1
-
         # Print the molecular gas mass
-        gas_mass_H2_totals = []
-        for container in [gal.sphere, ad]:
-            try:
-                cell_volumes = container[("index", "cell_volume")]
-                gas_mass_H2 = (
-                    2
-                    * container[("artio", "RT_HVAR_H2")]
-                    * sim.ds.arr(1, "code_mass/code_length**3")
-                    * cell_volumes
-                )
-                gas_mass_H2_totals.append(np.sum(gas_mass_H2.to("Msun").value))
-            except:
-                gas_mass_H2_totals.append(np.nan)
+        cell_volumes = ad[("index", "cell_volume")]
+        gas_mass_H2 = (
+            2
+            * ad[("artio", "RT_HVAR_H2")]
+            * sim.ds.arr(1, "code_mass/code_length**3")
+            * cell_volumes
+        )
 
-        write(run_name, sim.z, gas_mass_H2_totals[0], gas_mass_H2_totals[1])
+        write(run_name, sim.z, np.sum(gas_mass_H2.to("Msun").value))
+
+        del sim
+        del ad
+        gc.collect()
+
 
 out_file.close()
