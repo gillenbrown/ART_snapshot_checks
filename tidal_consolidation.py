3f4fcc7f9d1076ace8cf404f4e995d7c57a24af1
Gillen Brown <gillenb@umich.edu>
2021-09-28 13:17:25 -0400

    modified tidal outputs so I don't need to store all logs on shangrila at all times

--- tidal_consolidation.py
+++ tidal_consolidation.py
@@ -22,10 +22,28 @@ for tidal_file in all_tidal:
 
 # then combine these particles.
 for star, file_list in tqdm(star_files.items()):
-    with open(tidal_dir / f"{star}.txt", "w") as out:
-        for filename in sorted(file_list):
-            with open(filename, "r") as file:
-                for l in file:
-                    out.write(l)
+    out_file = tidal_dir / f"{star}.txt"
+    # first go through and determine which output files have already been
+    # written to this file
+    pre_existing_runs = []
+    if out_file.exists():  # can't read if it doesn't exist
+        with open(out_file, "r") as old_file:
+            for line in old_file:
+                if line.startswith("# run name"):
+                    pre_existing_runs.append(line.split()[-1])
+
+    # then go through and append to the file, if the current source has not
+    # already been written.
+    with open(out_file, "a") as full_tidal_file:
+        for individual_tidal_file in sorted(file_list):
+            # only write this if we haven't already added this log file
+            log_name = individual_tidal_file.parent.parent.name
+            if log_name not in pre_existing_runs:
+                # note that we're writing from this file
+                full_tidal_file.write(f"# run name {log_name}\n")
+                # then write all the tidal info from this file
+                with open(individual_tidal_file, "r") as file:
+                    for l in file:
+                        full_tidal_file.write(l)
 
 sentinel.touch()
