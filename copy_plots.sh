7eb4db2c3b1380e7d019e6c4f38e85c065bb3842
Gillen Brown <gillenb@umich.edu>
2022-05-05 13:29:44 -0400

    updated plot copying

--- copy_plots.sh
+++ copy_plots.sh
@@ -8,4 +8,5 @@ rm -r $local_dir
 # shangrila first
 scp -r gillenb@shangrila.astro.lsa.umich.edu:code/mine/ART_snapshot_checks/comparison_plots $local_dir
 
-# then Stampede2
+# then Stampede2. This uses the the globus command line interface
+globus transfer -r --label plots --notify failed ceea5ca0-89a9-11e7-a97f-22000a92523b:/work2/06912/tg862118/stampede2/ART_snapshot_checks_analysis/comparison_plots d7dd2e72-e628-11e9-893b-0ea49c25678c:$local_dir
