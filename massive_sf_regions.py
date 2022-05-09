1240502dc3c79180981218e7795cd1439a77b8f7
Gillen Brown <gillenb@umich.edu>
2022-05-09 11:50:43 -0400

    update the parsing  of the SF regions

--- massive_sf_regions.py
+++ massive_sf_regions.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import sys
 
+import numpy as np
 import betterplotlib as bpl
 
 from utils import plot_utils
@@ -28,31 +29,38 @@ def get_scale(filename):
     return filename[-10:-4]
 
 
-def parse_forming_clusters(file_location):
-    max_mass = 0
-    x, y, z = 0, 0, 0
+def find_max_in_column(file_location, col_idx):
+    """ Returns the index of the row with the highest value in the desired column """
+    max_value = -np.inf
+    max_idx = None
     with open(file_location, "r") as in_file:
-        for line in in_file:
+        for idx, line in enumerate(in_file):
             if line.startswith("#") or len(line.strip()) == 0:
                 continue
+            # try to get this index, but check to make sure the line is long enough
+            try:
+                if float(line[col_idx]) > float(max_value):
+                    max_value = line[col_idx]
+                    max_idx = idx
+            except IndexError:
+                return None
+    return max_idx
 
-            # we have a data line. There is an old version of this file without the
-            # appropriate info that we need to check against
-            split = line.split()
-            if len(split) == 6:
-                return 0, 0, 0, 0
-            elif len(split) != 10:
-                raise RuntimeError("Shouldn't happen.")
 
-            # then get the info and check
-            _, this_x, this_y, this_z, _, _, _, _, _, this_mass = split
-            if float(this_mass) > float(max_mass):
-                max_mass = this_mass
-                x = this_x
-                y = this_y
-                z = this_z
-
-    return x, y, z, max_mass
+def get_info_from_given_line(file_location, row_idx):
+    """ Get the position, mass, SFR, and average age of the cluster complex desired """
+    with open(file_location, "r") as in_file:
+        for idx, line in enumerate(in_file):
+            if idx == row_idx:
+                split_line = line.split()
+                return [
+                    split_line[1],  # x
+                    split_line[2],  # y
+                    split_line[3],  # z
+                    split_line[9],  # total mass within 30pc
+                    split_line[10],  # star formation rate
+                    split_line[11],  # average age
+                ]
 
 
 out_file = open(out_file_loc, "w")
@@ -71,6 +79,8 @@ out("# - Position x [kpc]")
 out("# - Position y [kpc]")
 out("# - Position z [kpc]")
 out("# - Mass of clusters younger than 15 Myr within 30 pc of this location [Msun]")
+out("# - Star formation rate within 30 pc of this cluster [Msun/year]")
+out("# - Ave age of clusters younger than 15 Myr within 30 pc of this cluster [Myr]")
 
 for dir in sim_dirs:
     check_files = sorted(
@@ -82,15 +92,22 @@ for dir in sim_dirs:
     )
 
     for file in check_files:
-        x, y, z, mass = parse_forming_clusters(file)
-        # note that these quantities are strings
-        out(
-            f"{plot_utils.get_sim_dirname(dir)} "
-            f"{get_scale(file.name)} "
-            f"{x} "
-            f"{y} "
-            f"{z} "
-            f"{mass} "
-        )
+        # for each file, print the cluster complex with the highest mass and highest
+        # SFR. Those are columns 9 and 10
+        for col_idx in [9, 10]:
+            idx = find_max_in_column(file, col_idx)
+            if idx is not None:  # if the value was found
+                x, y, z, mass, sfr, age = get_info_from_given_line(file, idx)
+                # note that these quantities are strings
+                out(
+                    f"{plot_utils.get_sim_dirname(dir)} "
+                    f"{get_scale(file.name)} "
+                    f"{x} "
+                    f"{y} "
+                    f"{z} "
+                    f"{mass} "
+                    f"{sfr} "
+                    f"{age} "
+                )
 
 out_file.close()
